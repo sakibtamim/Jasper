09c1af7fe42a8bfb8dc0bbdd8554006f76ebf2af
Arafat Zahan <kuasha420@gmail.com>
2025-11-23 08:35:11 +0600

    fix: add name property to alias commands for proper loading

--- src/utils/command-alias.ts
+++ src/utils/command-alias.ts
@@ -4,6 +4,7 @@ import { Command } from "../types/command.js";
 export function createAlias(name: string, description: string, originalCommand: Command): Command {
     return {
         data: {
+            name,
             toJSON: (): RESTPostAPIChatInputApplicationCommandsJSONBody => {
                 const json = originalCommand.data.toJSON() as RESTPostAPIChatInputApplicationCommandsJSONBody;
                 json.name = name;
