88abd5569a743924cdda8fb6f7bb4c40b724fd8f
Arafat Zahan <kuasha420@gmail.com>
2025-12-01 10:16:08 +0600

    Enable HMR for plugins in dev mode

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,15 +1,28 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-// import path from 'path';
-// import { fileURLToPath } from 'url';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-// const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
     plugins: [react()],
+    resolve: {
+        alias: {
+            '@plugins': path.resolve(__dirname, '../bot/src/plugins'),
+            'react': path.resolve(__dirname, './node_modules/react'),
+            'react-dom': path.resolve(__dirname, './node_modules/react-dom')
+        }
+    },
     publicDir: 'public/assets',
     base: '/',
     server: {
+        fs: {
+            allow: [
+                // Allow serving files from the project root
+                path.resolve(__dirname, '../..')
+            ]
+        },
         proxy: {
             '/api': {
                 target: 'http://localhost:3000',
