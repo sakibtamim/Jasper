f301bd427827a46cc51f32841faa2fbd882a1a56
Arafat Zahan <kuasha420@gmail.com>
2025-12-01 21:10:02 +0600

    fix: correct PM2 script path to dist/src/index.js

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
     apps: [
         {
             name: "Jasper",
-            script: "./apps/bot/dist/index.js",
+            script: "./apps/bot/dist/src/index.js",
             cwd: "./",
             env: {
                 NODE_ENV: "production",
